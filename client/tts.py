a90ec45a13c197cce471202c67feb15d576ba067
Brandon Marshall <kem5brandon@gmail.com>
2015-07-21 11:37:31 -0700

    windows compatibility
    changed mad to pyglet and avbin for cross platform media engine

--- client/tts.py
+++ client/tts.py
@@ -14,7 +14,9 @@ import tempfile
 import subprocess
 import pipes
 import logging
+import pyglet
 import wave
+import time
 import urllib
 import urlparse
 import requests
@@ -23,7 +25,6 @@ from abc import ABCMeta, abstractmethod
 import argparse
 import yaml
 try:
-    import mad
     import gtts
 except ImportError:
     pass
@@ -51,7 +52,8 @@ class AbstractTTSEngine(object):
     @classmethod
     @abstractmethod
     def is_available(cls):
-        return diagnose.check_executable('aplay')
+        return diagnose.check_python_import('pyglet')
+        #return diagnose.check_executable('aplay')
 
     def __init__(self, **kwargs):
         self._logger = logging.getLogger(__name__)
@@ -61,17 +63,9 @@ class AbstractTTSEngine(object):
         pass
 
     def play(self, filename):
-        # FIXME: Use platform-independent audio-output here
-        # See issue jasperproject/jasper-client#188
-        cmd = ['aplay', '-D', 'hw:1,0', str(filename)]
-        self._logger.debug('Executing %s', ' '.join([pipes.quote(arg)
-                                                     for arg in cmd]))
-        with tempfile.TemporaryFile() as f:
-            subprocess.call(cmd, stdout=f, stderr=f)
-            f.seek(0)
-            output = f.read()
-            if output:
-                self._logger.debug("Output was: '%s'", output)
+        self._logger.debug("Playback of file '%s' requested")
+        clip = pyglet.media.load(filename, streaming=False)
+        clip.play()
 
 
 class AbstractMp3TTSEngine(AbstractTTSEngine):
@@ -81,22 +75,10 @@ class AbstractMp3TTSEngine(AbstractTTSEngine):
     @classmethod
     def is_available(cls):
         return (super(AbstractMp3TTSEngine, cls).is_available() and
-                diagnose.check_python_import('mad'))
+                diagnose.check_python_import('pyglet'))
 
     def play_mp3(self, filename):
-        mf = mad.MadFile(filename)
-        with tempfile.NamedTemporaryFile(suffix='.wav') as f:
-            wav = wave.open(f, mode='wb')
-            wav.setframerate(mf.samplerate())
-            wav.setnchannels(1 if mf.mode() == mad.MODE_SINGLE_CHANNEL else 2)
-            # 4L is the sample width of 32 bit audio
-            wav.setsampwidth(4L)
-            frame = mf.read()
-            while frame is not None:
-                wav.writeframes(frame)
-                frame = mf.read()
-            wav.close()
-            self.play(f.name)
+        self.play(filename)
 
 
 class DummyTTS(AbstractTTSEngine):
@@ -117,7 +99,6 @@ class DummyTTS(AbstractTTSEngine):
     def play(self, filename):
         self._logger.debug("Playback of file '%s' requested")
         pass
-
 
 class EspeakTTS(AbstractTTSEngine):
     """
